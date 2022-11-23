e329c338829f8e141cfa17d12220640a98333677
Mauricio Gamarra <mauricio.gamarra.gbi@edglobo.com.br>
2022-11-23 19:11:35 -0300

    ok

--- script.sh
+++ script.sh
@@ -5,9 +5,9 @@ function log() {
     S=$1
     echo $S | sed 's/./& /g'
 }
-log $ACTIVE_KUBE_CONFIG
+log $KUBE_CONFIG
 
-echo "${ACTIVE_KUBE_CONFIG}" | base64 -d > /tmp/config1
+echo "${KUBE_CONFIG}" | base64 -d > /tmp/config1
 
 export KUBECONFIG=/tmp/config1
 kubectl version
