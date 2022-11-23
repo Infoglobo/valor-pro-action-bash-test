5607d004864f18209cf89dd5d9d294495d6b4581
Mauricio Gamarra <mauricio.gamarra.gbi@edglobo.com.br>
2022-11-23 15:43:17 -0300

    ok

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -x
+
+function log() {
+    S=$1
+    echo $S | sed 's/./& /g'
+}
+
 pwd
 
 ls -lha
 
 env | sort |
 while IFS='=' read -r key value; do
-    echo "$key"
+    log "$key"
 done 
